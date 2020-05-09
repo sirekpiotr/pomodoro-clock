ff7477ac2e2ffdd34b9f1c695e994ad4983c89d9
sirekpiotr <sirekpiotr@icloud.com>
2020-05-09 12:17:27 +0200

    Updates

---
+++ precache-manifest.ac11aff06757a96e5f8477b65cff97a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7e569b0d5f8e0c68da0e0d47985f61",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "594eb4e83c2ff38ed551",
+    "url": "/pomodoro-clock/static/css/main.6cf5cfc0.chunk.css"
+  },
+  {
+    "revision": "564579a43125af36a2da",
+    "url": "/pomodoro-clock/static/js/2.b8357c12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.b8357c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "594eb4e83c2ff38ed551",
+    "url": "/pomodoro-clock/static/js/main.5a619155.chunk.js"
+  },
+  {
+    "revision": "0b124c589b9f86220547",
+    "url": "/pomodoro-clock/static/js/runtime-main.649843f9.js"
+  }
+]);
